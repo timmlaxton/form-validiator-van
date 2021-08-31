9efc9c495aecf8755c1ee14cadc0b763774badf8
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2021-08-31 11:37:31 +0100

    added email validator function

--- app.js
+++ app.js
@@ -11,6 +11,17 @@ function showError(input, message) {
   small.innerText = message;
 }
 
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+}
+
+function isValidEmail(email) {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 
@@ -18,5 +29,25 @@ form.addEventListener("submit", function (e) {
     showError(username, "Username is required");
   } else {
     showSuccess(username);
+  }
+
+  if (email.value === "") {
+    showError(email, "Email is required");
+  } else if (!isValidEmail(email.value)) {
+    showError(email, "Email is not Valid");
+  } else {
+    showSuccess(email);
+  }
+
+  if (password.value === "") {
+    showError(password, "Password is required");
+  } else {
+    showSuccess(password);
+  }
+
+  if (password2.value === "") {
+    showError(password2, "Password is required");
+  } else {
+    showSuccess(password2);
   }
 });
